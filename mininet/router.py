e40dfab13da7855eaeb38e217a2dcccad214fbb4
matheusphalves <matheusphalves@gmail.com>
2021-12-12 17:54:50 -0300

    adicionados arquivos de entrega finais

--- mininet/router.py
+++ mininet/router.py
@@ -1,9 +1,10 @@
+# -- coding: utf-8 --
 from mininet.topo import Topo 
-#classe cria rede com 2 switches, e 3 hosts
+#classe cria rede com 2 switches, e 4 hosts
 #para executar no mininet, escreva: sudo mn --custom ./router.py --topo router
-class Router_Topo(Topo): 
+class Minimal_Topo(Topo): 
     def __init__(self): 
-        "Create P2P topology." 
+        "Create topology." 
  
         # Initialize topology 
         Topo.__init__(self) 
@@ -12,15 +13,17 @@ class Router_Topo(Topo):
         H1 = self.addHost('h1') 
         H2 = self.addHost('h2') 
         H3 = self.addHost('h3') 
+        H4 = self.addHost('h4') 
         S1 = self.addSwitch('s1') 
         S2 = self.addSwitch('s2') 
  
         # Add links 
         self.addLink(H1, S1) 
         self.addLink(H2, S1) 
-        self.addLink(H2, S2) 
         self.addLink(H3, S2) 
+        self.addLink(H4, S2) 
+        self.addLink(S1, S2)
  
 topos = { 
-        'router': (lambda: Router_Topo()) 
+        'router': (lambda: Minimal_Topo()) 
 } 
